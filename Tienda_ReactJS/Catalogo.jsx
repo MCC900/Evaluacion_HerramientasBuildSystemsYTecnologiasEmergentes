cfedfd7c54c85273e59b367e004e099721525593
J. Mariano Cis <mcc900@adinet.com.uy>
2019-04-20 21:39:34 -0300

    Página Catálogo faltante en el commit anterior

--- Tienda_ReactJS/Catalogo.jsx
+++ Tienda_ReactJS/Catalogo.jsx
@@ -1,9 +1,26 @@
 import React from 'react';
+import { Icon, Sizes, Cell, GridContainer, TopBar, TopBarLeft, TopBarRight, Menu, MenuItem } from 'react-foundation';
 
 class Catalogo extends React.Component {
   render(){
     return(
-      <h1>Catálogo</h1>
+      <div className="main">
+        <div className="fondo mainFondo"/>
+        <GridContainer>
+          <TopBar className="barraSuperior">
+            <TopBarLeft>
+              <h5>Catálogo de productos</h5>
+            </TopBarLeft>
+            <TopBarRight>
+              <Menu className="barraSuperior">
+                <MenuItem><a><Icon name="fi-thumbnails"></Icon> <span>Catálogo</span></a></MenuItem>
+                <MenuItem><a><Icon name="fi-shopping-cart"></Icon> <span>Carrito</span></a></MenuItem>
+                <MenuItem><a><Icon name="fi-x"></Icon> <span>Salir</span></a></MenuItem>
+              </Menu>
+            </TopBarRight>
+          </TopBar>
+        </GridContainer>
+      </div>
     )
   }
 }
